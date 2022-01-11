59fd94746a6017d454d2aec23bb6313fc8a20f9e
Yohei Shimada <0306you10@gmail.com>
2022-01-12 08:56:09 +0900

    Ver1comp

---
+++ app.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import random
+import calendar
+import os
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.datastructures import Authorization
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stationData.db'
+db = SQLAlchemy(app)
+
+class Station(db.Model):
+    __tablename__ = 'stationData'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    place = db.Column(db.String(30), nullable=False)
+    yomi = db.Column(db.String(30), nullable=False)
+
+@app.route('/')
+def index():
+    i = random.randint(1,8969)
+    station = Station.query.filter_by(id=i)
+    return render_template('index.html', station=station)
+
+@app.route('/check', methods=['POST'])
+def check():
+    check = request.form['answer']
+    colect = request.form['colect']
+    if check == colect:
+        comment = "正解"
+    else:
+        comment = "不正解"
+    answer =  check
+    colect = colect
+    print(answer,comment,colect)
+    return render_template('result.html', comment=comment, answer=answer, colect=colect)  
+
+if __name__ == '__main__':
+    port = os.getenv("PORT")
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
